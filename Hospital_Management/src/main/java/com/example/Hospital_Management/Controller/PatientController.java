7d690704d8ed265b59ac88f4516ae268411134f5
Harish V <vaddiharish2003@gmail.com>
2025-04-08 16:44:35 +0530

    Updated appointment scheduling logic and frontend UI

--- Hospital_Management/src/main/java/com/example/Hospital_Management/Controller/PatientController.java
+++ Hospital_Management/src/main/java/com/example/Hospital_Management/Controller/PatientController.java
@@ -1,5 +1,41 @@
 package com.example.Hospital_Management.Controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Hospital_Management.Entity.Patients;
+import com.example.Hospital_Management.Service.PatientService;
+
+@RestController
+@RequestMapping("/api/patients")
 public class PatientController {
+	
+	@Autowired
+	private PatientService patientService;
+	
+	@PostMapping("/Save")
+	public Patients savePatients(@RequestBody Patients patients) {
+		return patientService.savePatient(patients);
+	}
+	
+	@GetMapping("All")
+	public List<Patients>getAll(){
+		return patientService.getAllPatients();
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Patients>getById(@PathVariable Long id){
+		return patientService.getById(id);
+	}
+	
 
 }
--- Hospital_Management/src/main/java/com/example/Hospital_Management/Service/PatientService.java
+++ Hospital_Management/src/main/java/com/example/Hospital_Management/Service/PatientService.java
@@ -1,5 +1,28 @@
 package com.example.Hospital_Management.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.Hospital_Management.Entity.Patients;
+import com.example.Hospital_Management.Repository.PatientRepository;
+
+@Service
 public class PatientService {
+	
+	@Autowired
+	private PatientRepository patientRepo;
+	
+	public Patients savePatient(Patients patients) {
+		return patientRepo.save(patients);
+	}
+	public List<Patients>getAllPatients(){
+		return patientRepo.findAll();
+	}
+	public Optional<Patients>getById(Long id){
+		return patientRepo.findById(id);
+	}
 
 }
