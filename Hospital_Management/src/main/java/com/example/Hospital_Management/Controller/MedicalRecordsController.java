b2b27c5253c2ff112dd6bc60b62b0db7660f7f34
Harish V <vaddiharish2003@gmail.com>
2025-04-12 12:05:22 +0530

    updated Entire project

--- Hospital_Management/src/main/java/com/example/Hospital_Management/Controller/MedicalRecordsController.java
+++ Hospital_Management/src/main/java/com/example/Hospital_Management/Controller/MedicalRecordsController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,7 +41,23 @@ public class MedicalRecordsController {
 		}
 	}
 	@GetMapping("/{id}")
-	public Optional<Medical_Records>getByid(@PathVariable Long id){
-		return recordService.getById(id);
+	public ResponseEntity<?>geyByid(@PathVariable Long id){
+		Optional<Medical_Records>records=recordService.getById(id);
+		if(records.isPresent()) {
+			return ResponseEntity.ok(records);
+		}
+		else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Given id is Not Exists" +id);
+		}
+	}
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?>deleteByid(@PathVariable Long id){
+		boolean records=recordService.deleteById(id);
+		if(records) {
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body("Success fully deleted by given id" +id);
+		}
+		else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Given id is Not Deleted" +id);
+		}
 	}
 }
